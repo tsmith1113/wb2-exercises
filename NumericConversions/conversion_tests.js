6c390f4c781bfc7a4416254547107c6624d71386
Tracie Smith <90426413+tsmit655@users.noreply.github.com>
2024-04-24 11:07:09 -0400

    information added

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,40 @@
+"use strict";
+
+///Description: This script tests various numeric conversion techniques
+
+
+/// Define Variables
+
+var a = "101.1";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+//// display parseInt
+
+ a = parseInt(a);
+console.log(a);
+
+ b = parseInt(b);
+console.log(b);
+
+ c = parseInt(c);
+console.log(c);
+
+ d = parseInt(d);
+console.log(d);
+
+/// display parseFloat
+console.log("Input for float");
+a = parseFloat(a);
+console.log(a);
+
+ b = parseFloat(b);
+console.log(b);
+
+ c = parseFloat(c);
+console.log(c);
+
+ d = parseFloat(d);
+console.log(d);
+
---
+++ NumericConversions/number.js
@@ -0,0 +1,25 @@
+"use strict";
+
+///Description: This script tests various numeric conversion techniques
+
+
+/// Define Variables
+
+var a = "101.1";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+//// display number
+
+ a = Number(a);
+console.log(a);
+
+ b = Number(b);
+console.log(b);
+
+ c = Number(c);
+console.log(c);
+
+ d = Number(d);
+console.log(d);
